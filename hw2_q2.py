0370efa9591c9bbf00908eb4bcfd66dbf7eacac4
HagaiHargil <hagaihargil@gmail.com>
2020-03-16 12:07:06 +0200

    Initial commit

---
+++ hw2_q2.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from collections import namedtuple
+
+
+Type = Enum("Type", ("CURE", "HEALTHY", "SICK", "DYING", "DEAD"))
+Agent = namedtuple("Agent", ("name", "category"))
+
+
+def meetup(agent_listing: tuple) -> list:
+    """Model the outcome of the meetings of pairs of agents.
+
+    The pairs of agents are ((a[0], a[1]), (a[2], a[3]), ...). If there's an uneven
+    number of agents, the last agent will remain the same.
+
+    Notes
+    -----
+    The rules governing the meetings were described in the question. The outgoing
+    listing may change its internal ordering relative to the incoming one.
+
+    Parameters
+    ----------
+    agent_listing : tuple of Agent
+        A listing (tuple in this case) in which each element is of the Agent
+        type, containing a 'name' field and a 'category' field, with 'category' being
+        of the type Type.
+
+    Returns
+    -------
+    updated_listing : list
+        A list of Agents with their 'category' field changed according to the result
+        of the meeting.
+    """
